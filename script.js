3713345f49420817f6efc6793beeffa58114279d
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-02 14:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var outputEle=document.getElementById("#output");
+
+var promise1=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Hello, world!");
+	},1000)
+})
+
+promise1.then((data)=>{
+	outputEle.innerHTML=data;
+}).catch((error)=>{
+	console.log(error);
+})
